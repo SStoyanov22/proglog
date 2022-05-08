8d7223e7eca396a9755d54c3bc8c5993bdf28247
Stoyan Stoyanov <SStoyanov22@gmail.com>
2022-05-08 19:45:51 +0300

    Added ServeRequestsWithgRPC project

---
+++ ServeRequestsWithgRPC/interrnal/log/store.go
@@ -0,0 +1,125 @@
+package log
+
+import (
+	"bufio"
+	"encoding/binary"
+	"os"
+	"sync"
+)
+
+var (
+	enc = binary.BigEndian
+)
+
+const (
+	lenWidth = 8
+)
+
+type store struct {
+	*os.File
+	mu   sync.Mutex
+	buf  *bufio.Writer
+	size uint64
+}
+
+func newStore(f *os.File) (*store, error) {
+	fi, err := os.Stat(f.Name())
+
+	if err != nil {
+		return nil, err
+	}
+
+	size := uint64(fi.Size())
+
+	return &store{
+		File: f,
+		size: size,
+		buf:  bufio.NewWriter(f),
+	}, nil
+}
+
+/*
+Append([]byte) persists the given bytes to the store. We write the length of the
+record so that, when we read the record, we know how many bytes to read.
+We write to the buffered writer instead of directly to the file to reduce the
+number of system calls and improve performance. If a user wrote a lot of
+small records, this would help a lot. Then we return the number of bytes
+written, which similar Go APIs conventionally do, and the position where the
+store holds the record in its file. The segment will use this position when it
+creates an associated index entry for this record.
+*/
+func (s *store) Append(p []byte) (n uint64, pos uint64, err error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	pos = s.size
+
+	if err := binary.Write(s.buf, enc, uint64(len(p))); err != nil {
+		return 0, 0, err
+	}
+
+	w, err := s.buf.Write(p)
+	if err != nil {
+		return 0, 0, err
+	}
+
+	//w is the number of bytes written to the bufWriter and lenWidth(8 bytes = 64 bits) represents the size
+	// of the byte array we are appending to the bufWriter; thats why we increas w with 64 more bytes
+	w += lenWidth
+	s.size += uint64(w)
+	return uint64(w), pos, nil
+}
+
+/*
+Read(pos uint64) returns the record stored at the given position. First it flushes
+the writer buffer, in case we’re about to try to read a record that the buffer
+hasn’t flushed to disk yet. We find out how many bytes we have to read to
+get the whole record, and then we fetch and return the record. The compiler
+allocates byte slices that don’t escape the functions they’re declared in on the
+stack. A value escapes when it lives beyond the lifetime of the function call—if
+you return the value, for example.
+*/
+func (s *store) Read(pos uint64) ([]byte, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if err := s.buf.Flush(); err != nil {
+		return nil, err
+	}
+
+	size := make([]byte, lenWidth)
+	if _, err := s.File.ReadAt(size, int64(pos)); err != nil {
+		return nil, err
+	}
+
+	b := make([]byte, enc.Uint64(size))
+	if _, err := s.File.ReadAt(b, int64(pos+lenWidth)); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+/*
+ReadAt(p []byte, off int64) reads len(p) bytes into p beginning at the off offset in the
+store’s file. It implements io.ReaderAt on the store type.
+*/
+func (s *store) ReadAt(p []byte, off int64) (int, error) {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	if err := s.buf.Flush(); err != nil {
+		return 0, err
+	}
+
+	return s.File.ReadAt(p, off)
+}
+
+//Close() persists any buffered data before closing the file
+func (s *store) Close() error {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	err := s.buf.Flush()
+	if err != nil {
+		return err
+	}
+
+	return s.File.Close()
+}
--- WriteALogPackage/go.mod
+++ WriteALogPackage/go.mod
@@ -9,7 +9,15 @@ require (
 
 require (
 	github.com/davecgh/go-spew v1.1.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/go-cmp v0.5.6 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	golang.org/x/net v0.0.0-20201021035429-f5854403a974 // indirect
+	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
+	golang.org/x/text v0.3.3 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013 // indirect
+	google.golang.org/grpc v1.46.0 // indirect
 	google.golang.org/grpc/cmd/protoc-gen-go-grpc v1.2.0 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c // indirect
